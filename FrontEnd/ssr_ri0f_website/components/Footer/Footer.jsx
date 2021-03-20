8de960b383dc27e885085498bed8643f7f4aa749
Daniil Silin <daniil_silin@mail.ru>
2021-03-20 23:28:15 +0300

    🛠 Updated logo, Caddyfile and Dockerfile

--- FrontEnd/ssr_ri0f_website/components/Footer/Footer.jsx
+++ FrontEnd/ssr_ri0f_website/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import {FooterNavbar} from "./menu/FooterNavbar";
 import SideBar from "../common/SideBar";
 import useTranslation from 'next-translate/useTranslation'
+import Image from "next/image"
 
 const FooterContainer = styled.div`
   //height: 100%;
@@ -15,7 +16,6 @@ const FooterContainer = styled.div`
   @media (max-width: 870px) {
     height: 200px;
   }
-
 
   //Mobile menu
   .barsMenu {
@@ -76,6 +76,11 @@ const Logo = styled.div`
 
   border-radius: 5px;
 
+  @media (max-width: 320px) {
+    height: 96px;
+    width: 96px;
+  }
+
 
 `
 
@@ -99,15 +104,8 @@ export const Footer = () => {
                 <Row justify="space-between" align={'middle'} gutter={[0, 24]}>
                     <Col>
                         <Logo>
-                            <svg width="128" height="128" viewBox="0 0 512 512" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M262.047 275.109H222.391V354H186.812V149.25H258.812C282.438 149.25 300.672 154.547 313.516 165.141C326.359 175.734 332.781 191.062 332.781 211.125C332.781 224.812 329.453 236.297 322.797 245.578C316.234 254.766 307.047 261.844 295.234 266.812L341.219 352.172V354H303.109L262.047 275.109ZM222.391 246.562H258.953C270.953 246.562 280.328 243.562 287.078 237.562C293.828 231.469 297.203 223.172 297.203 212.672C297.203 201.703 294.062 193.219 287.781 187.219C281.594 181.219 272.312 178.125 259.938 177.938H222.391V246.562Z"
-                                    fill="#ECF0F1"/>
-                                <path
-                                    d="M512 256C512 397.385 397.385 512 256 512C114.615 512 0 397.385 0 256C0 114.615 114.615 0 256 0C397.385 0 512 114.615 512 256ZM25.6 256C25.6 383.246 128.754 486.4 256 486.4C383.246 486.4 486.4 383.246 486.4 256C486.4 128.754 383.246 25.6 256 25.6C128.754 25.6 25.6 128.754 25.6 256Z"
-                                    fill="#ECF0F1"/>
-                            </svg>
+                            <Image src={"/static/assets/newLogo.png"} width={128}
+                                   height={128}/>
 
                         </Logo>
                     </Col>
