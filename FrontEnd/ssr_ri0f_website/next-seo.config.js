7f52a6183e704153bcd87283adc0674d9734e46a
Daniil Silin <daniil_silin@mail.ru>
2021-03-20 23:16:56 +0300

    🛠 Fixed SEO, Footer, Donate Page link animation

--- FrontEnd/ssr_ri0f_website/next-seo.config.js
+++ FrontEnd/ssr_ri0f_website/next-seo.config.js
@@ -1,14 +1,14 @@
-const title = 'RI0F expedition';
-const description = 'RI0F Kurils expedition website';
+const title = 'RI0FF expedition';
+const description = 'RI0FF Kurils expedition website';
 
 export const SEO = {
     title,
     description,
-    canonical: 'https://ri0f.com',
+    canonical: 'https://ri0ff.com',
     openGraph: {
         type: 'website',
         locale: 'en_IE',
-        url: 'https://ri0f.com',
+        url: 'https://ri0ff.com',
         title,
         description,
     }
--- FrontEnd/ssr_ri0f_website/utils/SEO_headers.jsx
+++ FrontEnd/ssr_ri0f_website/utils/SEO_headers.jsx
@@ -1,10 +1,10 @@
 export const CHOSEN_NEWS_SEO = (lang, ActNews) => {
     return {
-        title: `RI0F | ${lang && lang === 'ru' ? ActNews.name_ru : ActNews.name_en}`,
+        title: `RI0FF | ${lang && lang === 'ru' ? ActNews.name_ru : ActNews.name_en}`,
         description: lang && lang === 'ru' ? ActNews.text_ru : ActNews.text_en,
 
         openGraph: {
-            title: `RI0F | ${lang && lang === 'ru' ? ActNews.name_ru : ActNews.name_en}`,
+            title: `RI0FF | ${lang && lang === 'ru' ? ActNews.name_ru : ActNews.name_en}`,
             description: lang && lang === 'ru' ? ActNews.text_ru : ActNews.text_en,
         }
     }
@@ -13,24 +13,24 @@ export const CHOSEN_NEWS_SEO = (lang, ActNews) => {
 // Main page
 export const MAIN_SEO = (lang, ru, eng) => {
     return {
-        title: `RI0F | ${lang && lang === 'ru' ? ru.head : eng.head}`,
+        title: `RI0FF | ${lang && lang === 'ru' ? ru.head : eng.head}`,
         description: lang && lang === 'ru' ? ru.description : eng.description,
 
         openGraph: {
-            title: `RI0F | ${lang && lang === 'ru' ? ru.head : eng.head}`,
+            title: `RI0FF | ${lang && lang === 'ru' ? ru.head : eng.head}`,
             description: lang && lang === 'ru' ? ru.description : eng.description,
         }
     }
 }
 
 export const ADMIN_SEO = {
-    title: `RI0F | Панель управления`,
+    title: `RI0FF | Панель управления`,
     description: 'Go home',
     nofollow: true,
     noindex: true,
 
     openGraph: {
-        title: `RI0F | Панель управления`,
+        title: `RI0FF | Панель управления`,
         description: 'Go home',
     }
 }
