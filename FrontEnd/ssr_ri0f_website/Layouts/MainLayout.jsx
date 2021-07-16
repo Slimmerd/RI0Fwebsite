16f2bcc2cbef5d2a79eb713005d846a0d661da99
Daniil Silin <daniil_silin@mail.ru>
2021-07-16 18:43:58 +0300

    🤡 Deleted unused styling

--- FrontEnd/ssr_ri0f_website/Layouts/MainLayout.jsx
+++ FrontEnd/ssr_ri0f_website/Layouts/MainLayout.jsx
@@ -1,6 +1,5 @@
 import {Footer} from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-import "../styles/antd-custom.less";
 
 export function MainLayout({children}) {
     return (
