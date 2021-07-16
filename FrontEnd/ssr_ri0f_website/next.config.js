e996f843e665d517289d520395c2637bcd816a4c
Daniil Silin <daniil_silin@mail.ru>
2021-07-16 22:35:58 +0300

    ♿️ Fixed antd version 3

--- FrontEnd/ssr_ri0f_website/next.config.js
+++ FrontEnd/ssr_ri0f_website/next.config.js
@@ -1,21 +1,42 @@
-const withAntdLess = require('next-plugin-antd-less');
 const nextTranslate = require('next-translate')
+const lessToJS = require('less-vars-to-js')
+const withLess = require('@zeit/next-less')
+const fs = require('fs')
+const path = require('path')
+
+const themeVariables = lessToJS(
+    fs.readFileSync(path.resolve(__dirname, './styles/antd-custom.less'), 'utf8')
+)
 
 module.exports = nextTranslate(
-    withAntdLess({
-        lessVarsFilePath: './styles/antd-custom.less',
-        // lessVarsFilePathAppendToEndOfContent: true,
-
-        env: {
-            REACT_APP_APIKEY: process.env.SERVER_API,
-            REACT_APP_BASEURL: process.env.SITE_URL,
-            REACT_APP_BACKEND_ADDRESS: process.env.BACKEND_URL,
-            ENV: process.env.ENV,
-            GA_TRACKING_ID: process.env.GA_ID
+    withLess({
+        optimization: {minimize: true},
+        lessLoaderOptions: {
+            javascriptEnabled: true,
+            modifyVars: themeVariables, // make your antd custom effective
         },
 
-        webpack(config) {
-            return config;
+        webpack: (config, {isServer}) => {
+            if (isServer) {
+                const antStyles = /antd\/.*?\/style.*?/
+                const origExternals = [...config.externals]
+                config.externals = [
+                    (context, request, callback) => {
+                        if (request.match(antStyles)) return callback()
+                        if (typeof origExternals[0] === 'function') {
+                            origExternals[0](context, request, callback)
+                        } else {
+                            callback()
+                        }
+                    },
+                    ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+                ]
+                config.module.rules.unshift({
+                    test: antStyles,
+                    use: 'null-loader',
+                })
+            }
+            return config
         },
-
-    }))
+    })
+);
