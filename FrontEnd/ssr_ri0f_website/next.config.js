a78b9b870476661ef003ed93c1b8e048b65ffc82
Daniil Silin <daniil_silin@mail.ru>
2021-07-16 23:04:30 +0300

    ♿️ Fixed antd version 4

--- FrontEnd/ssr_ri0f_website/next.config.js
+++ FrontEnd/ssr_ri0f_website/next.config.js
@@ -16,6 +16,14 @@ module.exports = nextTranslate(
             modifyVars: themeVariables, // make your antd custom effective
         },
 
+        env: {
+            REACT_APP_APIKEY: process.env.SERVER_API,
+            REACT_APP_BASEURL: process.env.SITE_URL,
+            REACT_APP_BACKEND_ADDRESS: process.env.BACKEND_URL,
+            ENV: process.env.ENV,
+            GA_TRACKING_ID: process.env.GA_ID
+        },
+
         webpack: (config, {isServer}) => {
             if (isServer) {
                 const antStyles = /antd\/.*?\/style.*?/
