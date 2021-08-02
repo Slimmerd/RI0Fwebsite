9dc130353ef9b9daa8999ca020309ebf33316da9
Daniil Silin <daniil_silin@mail.ru>
2021-08-02 13:41:11 +1100

    ♻️ Updated online log page

--- FrontEnd/ssr_ri0f_website/components/OnlineLogPage/Blocks/Stub_onlinelogpage.jsx
+++ FrontEnd/ssr_ri0f_website/components/OnlineLogPage/Blocks/Stub_onlinelogpage.jsx
@@ -1,80 +1,84 @@
 import React from 'react'
 import styled from "styled-components";
-import {FadeInContainer} from "../../common/FadeInAnimation";
-import {useSpring, animated} from 'react-spring'
 import useTranslation from 'next-translate/useTranslation'
 
 const MainDiv = styled.div`
-    height: 650px;
-    
-    display: flex;
-    align-items: center;
-    
-    .block{
-      background: #2C3E50;
-      border-radius: 15px;
-      box-shadow: 31.2195px 25.0069px 80px rgba(0, 0, 0, 0.07), 20.2349px 16.2081px 46.8519px rgba(0, 0, 0, 0.0531481), 12.0253px 9.63227px 25.4815px rgba(0, 0, 0, 0.0425185), 6.2439px 5.00137px 13px rgba(0, 0, 0, 0.035), 2.54381px 2.0376px 6.51852px rgba(0, 0, 0, 0.0274815), 0.578139px 0.46309px 3.14815px rgba(0, 0, 0, 0.0168519);
-    }
-    
-    .text{
+  height: 650px;
+
+  display: flex;
+  align-items: center;
+
+  .block {
+    background: #2C3E50;
+    border-radius: 15px;
+    box-shadow: 31.2195px 25.0069px 80px rgba(0, 0, 0, 0.07), 20.2349px 16.2081px 46.8519px rgba(0, 0, 0, 0.0531481), 12.0253px 9.63227px 25.4815px rgba(0, 0, 0, 0.0425185), 6.2439px 5.00137px 13px rgba(0, 0, 0, 0.035), 2.54381px 2.0376px 6.51852px rgba(0, 0, 0, 0.0274815), 0.578139px 0.46309px 3.14815px rgba(0, 0, 0, 0.0168519);
+  }
+
+  .text {
+    padding: 25px 0 25px 0;
     font-family: Oswald, sans-serif;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 96px;
-  line-height: 142px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 96px;
+    line-height: 112px;
 
-  color: #FFFFFF;
+    color: #ECF0F1;
+    text-decoration: none;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-image: linear-gradient(to right,
+    #ff0043,
+    #ff0043 50%,
+    #ECF0F1 50%);
+    background-size: 200% 100%;
+    background-position: 100%;
+    transition: background-position 0.5s ease;
+  }
+
+  .text:hover {
+    background-position: 0
+  }
+
+  @media (max-width: 992px) {
+    .text {
+      font-size: 72px;
+      line-height: 81px;
     }
-    
- @media (max-width: 1200px){
-  
-}
-
-@media (max-width: 992px){
-  
-}
-
-@media (max-width: 768px){
-  
-}
-
-@media (max-width: 576px){
-  .text{
-  padding: 15px 0 15px 0;
-  font-size: 64px;
-  line-height: 68px;
   }
-}
 
-@media (max-width:375px){
-  .text{
-  padding: 15px 0 15px 0;
-  font-size: 48px;
-  line-height: 52px;
+  @media (max-width: 768px) {
+    .text {
+      font-size: 64px;
+      line-height: 72px;
+    }
   }
-}
+
+  @media (max-width: 576px) {
+    .text {
+      font-size: 56px;
+      line-height: 64px;
+    }
+  }
+
+  @media (max-width: 376px) {
+    .text {
+      font-size: 48px;
+      line-height: 56px;
+    }
+  }
 `
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.05]
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 export const OnlineLogPageStub = () => {
     const {t} = useTranslation()
-    const [props, set] = useSpring(() => ({xys: [0, 0, 1], config: {mass: 10, tension: 350, friction: 40}}))
+
     return (
         <MainDiv>
             <div className={'container'}>
-                <FadeInContainer>
-                    <animated.div
-                        className="card"
-                        onMouseMove={({clientX: x, clientY: y}) => set({xys: calc(x, y)})}
-                        onMouseLeave={() => set({xys: [0, 0, 1]})}
-                        style={{transform: props.xys.interpolate(trans)}}
-                    >
-                        <div className={'block'}>
-                            <div className={'text'}>{t('onlinelog:stub_block.heading')}</div>
-                        </div>
-                    </animated.div>
-                </FadeInContainer>
+                <a href="https://clublog.org/logsearch/RI0FF" target="_blank" rel="noopener noreferrer">
+                    <div className={'block'}>
+                        <div className={'text'}>{t('onlinelog:stub_block.heading')}</div>
+                    </div>
+                </a>
             </div>
         </MainDiv>
     )
--- FrontEnd/ssr_ri0f_website/pages/onlinelog/index.jsx
+++ FrontEnd/ssr_ri0f_website/pages/onlinelog/index.jsx
@@ -1,22 +1,19 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {OnlineLogPageNaming} from "../../components/OnlineLogPage/Blocks/Naming_onlinelogpage";
 import {OnlineLogPageStub} from "../../components/OnlineLogPage/Blocks/Stub_onlinelogpage";
-import {OnlineLogTable} from "../../components/OnlineLogPage/Blocks/Table_onlinelog";
 import {NextSeo} from "next-seo";
 import {MAIN_SEO} from "../../utils/SEO_headers";
 import useTranslation from "next-translate/useTranslation";
 import SEOHeaders from "../../utils/seo_headers.json"
 
-//TODO: Change when table will be uploaded (change state from admin panel)
 const OnlineLogPage = () => {
     const {lang} = useTranslation()
-    const [stub, showStub] = useState(true)
 
     return (
         <>
             <NextSeo {...MAIN_SEO(lang, SEOHeaders.onlinelog.ru, SEOHeaders.onlinelog.en)}/>
             <OnlineLogPageNaming/>
-            {stub ? <OnlineLogPageStub/> : <OnlineLogTable/>}
+            <OnlineLogPageStub/>
         </>
     )
 }
